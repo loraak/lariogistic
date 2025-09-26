307fa74fdff328aa6bb93e143916f194c12011cf
Karol Arvizu <lorakarvizu@gmail.com>
2025-09-25 20:57:26 -0600

    Header ahora si jjj

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <nav>
-      <Link to="/">Inicio</Link> |{" "}
-      <Link to="/gestiones">Gestiones</Link>
     </nav>
   );
 }

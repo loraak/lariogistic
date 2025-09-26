72931056caab5abd9d58b2907ed73fea5cc29013
Karol Arvizu <lorakarvizu@gmail.com>
2025-09-25 20:26:08 -0600

    Header bonito

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom';
+import styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <nav>
-      <Link to="/">Inicio</Link> |{" "}
-      <Link to="/gestiones">Gestiones</Link>
-    </nav>
+    <header>
+      <nav className={styles.header}>
+        <div className={styles.logo}>
+          <h1>Lariogistic</h1>
+        </div>
+        <div className={styles.navLinks}>
+          <Link to="/" className={styles.link}>Inicio</Link>
+          <Link to="/gestiones" className={styles.link}>Gestiones</Link>
+        </div>
+      </nav>
+    </header>
   );
 }
 
